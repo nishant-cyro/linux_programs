1d97a064b4295f22daf5720f9360ce7c3cf98bc5
Nishant <nishant@Nishant-PC.(none)>
2012-10-31 10:59:57 +0530

    script basic changed

--- script_basic/basic_script.sh
+++ script_basic/basic_script.sh
@@ -1,3 +1,3 @@
 echo "the path to home is : $HOME"
 echo "the type of terminal is : $TERM"
-echo "the process should spawned at runlevel : `who -p`"
+echo "the process should spawned at runlevel : `ls /etc/rc3.d | grep 'S'`"
